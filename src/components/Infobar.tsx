36888177eba43b0a4be97009e537d27c8d84d3d8
Kevin Molid <kevinmolid@gmail.com>
2024-09-26 17:26:40 +0200

    Created reusable Tooltip comp

--- src/components/Infobar.tsx
+++ src/components/Infobar.tsx
@@ -1,23 +1,31 @@
+import Tooltip from "./Tooltip";
+
 const Infobar = () => {
   return (
     <section className="bg-neutral-700 px-8 py-2 flex gap-6 justify-center text-stone-400">
       <div className="flex items-center gap-2">
         <i className="fa-solid fa-heart"></i>
-        <div className="bg-neutral-800 h-2 w-36">
-          <div className="bg-green-500 h-2 w-36"></div>
-        </div>
+        <Tooltip label="Health bar">
+          <div className="bg-neutral-800 h-2 w-36">
+            <div className="bg-green-500 h-2 w-36"></div>
+          </div>
+        </Tooltip>
       </div>
 
       <div className="flex items-center gap-2">
         <p className="text-sm font-bold">XP</p>
-        <div className="bg-neutral-800 h-2 w-36">
-          <div className="bg-gray-400 h-2 w-3/4"></div>
-        </div>
+        <Tooltip label="Experience points">
+          <div className="bg-neutral-800 h-2 w-36">
+            <div className="bg-gray-400 h-2 w-3/4"></div>
+          </div>
+        </Tooltip>
       </div>
 
       <div className="flex items-center gap-2">
         <i className="fa-solid fa-location-dot"></i>
-        <p>New York City</p>
+        <Tooltip label="Your location">
+          <p>New York City</p>
+        </Tooltip>
       </div>
 
       <div className="flex items-center gap-2">
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, useState } from "react";
+
+interface TooltipInterface {
+  children: ReactNode;
+  label: string;
+}
+
+const Tooltip = ({ children, label }: TooltipInterface) => {
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, 400);
+  };
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
+
+  return (
+    <div
+      className="relative inline-block"
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {children}
+      {active && (
+        <span className="absolute bg-neutral-800 border border-neutral-600 px-2 py-1">
+          {label}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
