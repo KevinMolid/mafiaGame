dd212d8c4da75bb3a3a9712497ad4a5b2375efe8
Kevin Molid <kevinmolid@gmail.com>
2024-09-26 22:25:01 +0200

    Initializing Firebase and Auth

---
+++ src/firebaseConfig.tsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwCWRQds4dMD0lbxNBulGz9rHMubZ1yIY",
+  authDomain: "mafiareign-da8a1.firebaseapp.com",
+  projectId: "mafiareign-da8a1",
+  storageBucket: "mafiareign-da8a1.appspot.com",
+  messagingSenderId: "418232657339",
+  appId: "1:418232657339:web:2b66d098a1f78690a5909e",
+  measurementId: "G-D7HKJB00EF",
+};
+
+export default firebaseConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
+// React
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// Components
 import App from "./App.tsx";
 import "./index.css";
+
+// Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+import firebaseConfig from "./firebaseConfig.tsx";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
