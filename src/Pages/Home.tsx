2ad21cb26db95fa6eb921887b6e58e0692723c23
Kevin Molid <kevinmolid@gmail.com>
2024-09-27 20:38:21 +0200

    Added dynamic health bar at Home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,12 +7,25 @@ import H1 from "../components/Typography/H1";
 const Home = () => {
   const { character } = useCharacter();
 
+  const maxHealth = 100;
+  const healthPercentage = character
+    ? (character.stats.hp / maxHealth) * 100
+    : 0;
+
   return (
     <div>
       {character ? <H1>Welcome {character.username}!</H1> : <H1>Welcome!</H1>}
       {character ? (
         <>
-          <p>Health: {character.stats.hp}</p>
+          <p>
+            Health: {character.stats.hp} / {healthPercentage}
+          </p>
+          <div className="h-1 w-[200px] bg-neutral-700">
+            <div
+              className="h-1 bg-green-500 transition-all duration-300"
+              style={{ width: `${healthPercentage}%` }}
+            ></div>
+          </div>
           <p>Experience: {character.stats.exp}</p>
           <p>Reputation - Police: {character.reputation.police}</p>
         </>
