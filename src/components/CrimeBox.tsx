d8df5c9caadb9318316946469685833a77722795
Kevin Molid <kevinmolid@gmail.com>
2024-09-28 15:20:09 +0200

    Fixed hover transition on CrimeBox

--- src/components/CrimeBox.tsx
+++ src/components/CrimeBox.tsx
@@ -7,13 +7,13 @@ interface CrimeBoxInterface {
 
 const CrimeBox = ({ children, img }: CrimeBoxInterface) => {
   return (
-    <article className="grid grid-cols-1 grid-rows-1 w-72 h-40 bg-neutral-950 border border-neutral-600 hover:border-neutral-300 items-center text-center rounded-lg">
+    <article className="grid grid-cols-1 grid-rows-1 w-72 h-40 bg-neutral-950 items-center text-center rounded-lg">
       <img
         className="w-full h-full object-cover col-start-1 row-start-1 rounded-lg"
         src={img}
         alt=""
       />
-      <div className="w-full h-full bg-black/60 hover:bg-black/30 col-start-1 row-start-1 rounded-lg hover:cursor-pointer"></div>
+      <div className="w-full h-full bg-black/60 hover:bg-black/30 col-start-1 row-start-1 rounded-lg hover:cursor-pointer transition-all border border-neutral-600 hover:border-neutral-300"></div>
       <p className="col-start-1 row-start-1 text-2xl font-bold tracking-wider pointer-events-none">
         {children}
       </p>
