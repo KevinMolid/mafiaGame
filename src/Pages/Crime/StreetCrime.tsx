71dd3cd780c2fbf5d7bac89d20a03eba3ce7c34d
Kevin Molid <kevinmolid@gmail.com>
2024-09-28 17:50:12 +0200

    Dynamic styling of InfoBox

--- src/Pages/Crime/StreetCrime.tsx
+++ src/Pages/Crime/StreetCrime.tsx
@@ -1,11 +1,25 @@
 import H1 from "../../components/Typography/H1";
 import Button from "../../components/Button";
 import CrimeBox from "../../components/CrimeBox";
+import InfoBox from "../../components/InfoBox";
+
+import { useState } from "react";
 
 const StreetCrime = () => {
+  const [message, setMessage] = useState("You did this and earned $34");
+  const [messageType, setMessageType] = useState<
+    "success" | "failure" | "important" | "warning" | "info"
+  >("success");
+
+  function handleClick() {
+    setMessage("Something was clicked!");
+    setMessageType("warning");
+  }
+
   return (
     <section>
       <H1>Street Crimes</H1>
+      <InfoBox type={messageType}>{message}</InfoBox>
       <p className="mb-4 text-stone-400 font-medium">Select Crime:</p>
       <div className="grid grid-cols-[min-content_auto] gap-2 mb-4">
         <CrimeBox img="src\assets\PickpocketBw.png" heading="Pickpocket">
@@ -24,7 +38,7 @@ const StreetCrime = () => {
           Compete in local a street race
         </CrimeBox>
       </div>
-      <Button onClick={() => console.log("hehe")}>Commit Crime</Button>
+      <Button onClick={handleClick}>Commit Crime</Button>
     </section>
   );
 };
---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+interface InfoBoxInterface {
+  children: ReactNode;
+  type: "success" | "failure" | "important" | "warning" | "info";
+}
+
+const InfoBox = ({ children, type }: InfoBoxInterface) => {
+  const types = {
+    success: "bg-green-300 border-green-600 text-green-800",
+    failure: "bg-red-300 border-red-600 text-red-800",
+    important: "bg-blue-300 border-blue-600 text-blue-800",
+    warning: "bg-yellow-300 border-yellow-600 text-yellow-800",
+    info: "bg-slate-300 border-slate-600 text-slate-800",
+  };
+
+  // Fallback to 'red' if color is not in the defined colorClasses
+  const selectedType = types[type];
+
+  return (
+    <div className={`text-lg py-2 px-4 rounded-lg mb-6 ${selectedType}`}>
+      {children}
+    </div>
+  );
+};
+
+export default InfoBox;
