be94a735bbefffd12e7d8ebfac673a982720fe65
Kevin Molid <kevinmolid@gmail.com>
2024-09-28 13:09:04 +0200

    Created Equipment grid

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,6 +3,7 @@ import { useCharacter } from "../CharacterContext";
 
 // Components
 import H1 from "../components/Typography/H1";
+import Equipment from "../components/Equipment";
 
 const Home = () => {
   const { character } = useCharacter();
@@ -12,23 +13,58 @@ const Home = () => {
     ? (character.stats.hp / maxHealth) * 100
     : 0;
 
+  const maxXp = 500;
+  const xpPercentage = character ? (character.stats.xp / maxXp) * 100 : 0;
+
+  const maxHeat = 100;
+  const heatPercentage = character ? (character.stats.heat / maxHeat) * 100 : 0;
+
   return (
     <div>
       {character ? <H1>Welcome {character.username}!</H1> : <H1>Welcome!</H1>}
+
       {character ? (
-        <>
-          <p>
-            Health: {character.stats.hp} / {healthPercentage}
-          </p>
-          <div className="h-1 w-[200px] bg-neutral-700">
-            <div
-              className="h-1 bg-green-500 transition-all duration-300"
-              style={{ width: `${healthPercentage}%` }}
-            ></div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <p>
+              Health: {character.stats.hp} / {healthPercentage}
+            </p>
+            <div className="h-1 w-[250px] bg-neutral-700">
+              <div
+                className="h-1 bg-green-500 transition-all duration-300"
+                style={{ width: `${healthPercentage}%` }}
+              ></div>
+            </div>
           </div>
-          <p>Experience: {character.stats.exp}</p>
-          <p>Reputation - Police: {character.reputation.police}</p>
-        </>
+
+          <div className="flex flex-col gap-1">
+            <p>Experience: {character.stats.xp}</p>
+            <div className="bg-neutral-700 h-1 w-[250px]">
+              <div
+                className="h-1 bg-slate-400 transition-all duration-300"
+                style={{ width: `${xpPercentage}%` }}
+              ></div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p>Heat: {character.stats.heat} / 100</p>
+            <div className="bg-neutral-700 h-1 w-[250px]">
+              <div
+                className="h-1 bg-red-400 transition-all duration-300"
+                style={{ width: `${heatPercentage}%` }}
+              ></div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <h2>Reputation</h2>
+            <p>Police Force: {character.reputation.police}</p>
+          </div>
+
+          <h2>Equipment</h2>
+          <Equipment />
+        </div>
       ) : (
         <p>No character selected</p>
       )}
---
+++ src/components/Equipment.tsx
@@ -0,0 +1,32 @@
+const Equipment = () => {
+  return (
+    <section className="grid grid-cols-5 grid-rows-5 h-[250px] w-[250px] bg-neutral-800 gap-1 p-2">
+      <div></div>
+      <div></div>
+      <div className="border border-neutral-600 bg-neutral-900">H</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div className="border border-neutral-600 bg-neutral-900">N</div>
+      <div></div>
+      <div></div>
+      <div className="border border-neutral-600 bg-neutral-900">R1</div>
+      <div className="border border-neutral-600 bg-neutral-900">R2</div>
+      <div className="border border-neutral-600 bg-neutral-900">T</div>
+      <div></div>
+      <div className="border border-neutral-600 bg-neutral-900">G</div>
+      <div className="border border-neutral-600 bg-neutral-900 col-span-2 row-span-2">
+        W
+      </div>
+      <div className="border border-neutral-600 bg-neutral-900">L</div>
+      <div></div>
+      <div></div>
+      <div className="border border-neutral-600 bg-neutral-900">F</div>
+      <div></div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Equipment;
