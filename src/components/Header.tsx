4e5c61263d6ca548383b9db1a4d7a607f03e2269
Kevin Molid <kevinmolid@gmail.com>
2024-10-04 10:16:00 +0200

    Added actions menu on small screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,6 +13,7 @@ import { useAuth } from "../AuthContext";
 const Header = () => {
   const { userData } = useAuth();
   const [menuOpen, setMenuOpen] = useState(false);
+  const [actionsOpen, setActionsOpen] = useState(false);
   const auth = getAuth();
 
   // Sign out
@@ -29,13 +30,72 @@ const Header = () => {
   // Toggle the menu open/close state
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+    setActionsOpen(false);
+  };
+
+  // Toggle the menu open/close state
+  const toggleActions = () => {
+    setActionsOpen(!actionsOpen);
+    setMenuOpen(false);
   };
 
   return (
-    <header className="bg-neutral-950 px-4 sm:px-8 py-4 flex justify-between items-center">
+    <header className="bg-neutral-950 px-2 sm:px-8 py-2 sm:py-4 flex justify-between items-center">
+      {/* Menu icon for small screens */}
+      <div
+        className="flex justify-center items-center rounded-md sm:hidden w-12 h-12 bg-neutral-700 hover:cursor-pointer"
+        onClick={toggleActions}
+      >
+        <i className="text-3xl fa-solid fa-location-crosshairs"></i>
+      </div>
+
+      {/* Small screen dropdown menu */}
+      {actionsOpen && (
+        <nav className="absolute top-16 left-0 bg-neutral-950 p-4 sm:hidden z-10">
+          <p>Actions</p>
+          <hr className="border-neutral-500 mb-2" />
+          <ul className="flex flex-col gap-2 text-stone-400">
+            <li className="hover:text-stone-200">
+              <Link to="/about" onClick={() => setMenuOpen(false)}>
+                About
+              </Link>
+            </li>
+            <li className="hover:text-stone-200">
+              <Link to="/forum" onClick={() => setMenuOpen(false)}>
+                Forum
+              </Link>
+            </li>
+            <li className="hover:text-stone-200">
+              <Link to="/leaderboard" onClick={() => setMenuOpen(false)}>
+                Leaderboard
+              </Link>
+            </li>
+            {userData ? (
+              <li className="hover:text-stone-200">
+                <button
+                  onClick={() => {
+                    logOut();
+                    setMenuOpen(false);
+                  }}
+                >
+                  Log out
+                </button>
+              </li>
+            ) : (
+              <li className="hover:text-stone-200">
+                <Link to="/signup" onClick={() => setMenuOpen(false)}>
+                  Log in / Sign up
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      )}
+
+      {/* Logo */}
       <Link to="/">
         <img
-          className="h-14"
+          className="h-12 sm:h-14"
           src={logo}
           alt="MafiaReign Logo: Fight for Dominance"
         />
@@ -51,8 +111,10 @@ const Header = () => {
 
       {/* Small screen dropdown menu */}
       {menuOpen && (
-        <nav className="absolute top-16 right-8 bg-neutral-950 p-4 rounded-md sm:hidden z-10">
-          <ul className="flex flex-col gap-4 text-stone-400">
+        <nav className="absolute top-16 right-0 bg-neutral-950 p-4 sm:hidden z-10">
+          <p>Menu</p>
+          <hr className="border-neutral-500 mb-2" />
+          <ul className="flex flex-col gap-2 text-stone-400">
             <li className="hover:text-stone-200">
               <Link to="/about" onClick={() => setMenuOpen(false)}>
                 About
