27eaab20cfbd32be8e66758503da54d42d2a7db1
Kevin Molid <kevinmolid@gmail.com>
2024-10-10 13:36:58 +0200

    Assassinate: Kill players (100% success)

--- src/Pages/Assassinate.tsx
+++ src/Pages/Assassinate.tsx
@@ -2,17 +2,68 @@ import H1 from "../components/Typography/H1";
 import H2 from "../components/Typography/H2";
 import Button from "../components/Button";
 import InfoBox from "../components/InfoBox";
+
+// React
 import { useState } from "react";
 
-const Assassinate = () => {
-  const [message, setMessage] = useState("");
-  const [messageType, setMessageType] = useState<"success" | "failure">(
-    "failure"
-  );
+// Firebase
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
-  const killPlayer = () => {
-    setMessage("Player killed");
-    setMessageType("success");
+const db = getFirestore();
+
+const Assassinate = () => {
+  const [targetPlayer, setTargetPlayer] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState<
+    "success" | "failure" | "warning" | "info"
+  >("info");
+
+  // Function to handle assassination
+  const killPlayer = async () => {
+    if (!targetPlayer) {
+      setMessage("Please enter a player name.");
+      setMessageType("warning");
+      return;
+    }
+
+    try {
+      // Query the "Characters" collection for a player with the matching username
+      const q = query(
+        collection(db, "Characters"),
+        where("username", "==", targetPlayer)
+      );
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        // No player found
+        setMessage(`Player ${targetPlayer} does not exist.`);
+        setMessageType("warning");
+      } else {
+        // Assassinate
+        setMessage(`Player ${targetPlayer} was successfully assassinated!`);
+        setMessageType("success");
+
+        const targetDocId = querySnapshot.docs[0].id;
+        await updateDoc(doc(db, "Characters", targetDocId), { status: "dead" });
+      }
+    } catch (error) {
+      // Handle any errors during the query
+      console.error("Error checking target player:", error);
+      setMessage("An error occurred while trying to assassinate the player.");
+      setMessageType("failure");
+    }
+  };
+
+  const handleTargetInput = (event: any) => {
+    setTargetPlayer(event.target.value);
   };
 
   return (
@@ -24,6 +75,8 @@ const Assassinate = () => {
         <input
           type="text"
           placeholder="Player name"
+          value={targetPlayer}
+          onChange={handleTargetInput}
           className="bg-neutral-700 px-4 py-2 text-white"
         />
         <Button onClick={killPlayer}>Kill player</Button>
