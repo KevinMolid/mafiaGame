cd65d535c62b93415f45d494da394c81bafdede8
Kevin Molid <kevinmolid@gmail.com>
2024-10-11 00:13:21 +0200

    Added Notes page

---
+++ src/Pages/Notes.tsx
@@ -0,0 +1,12 @@
+import H2 from "../components/Typography/H2";
+
+const Notes = () => {
+  return (
+    <div>
+      <H2>Notes</H2>
+      <p>Here are your notes...</p>
+    </div>
+  );
+};
+
+export default Notes;
