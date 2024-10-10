601d608feb45a60ebdd4e69fd055e15e7f67dcb6
Kevin Molid <kevinmolid@gmail.com>
2024-10-10 17:16:53 +0200

    Created Admin box in CharacterList

--- src/Pages/Admin.tsx
+++ src/Pages/Admin.tsx
@@ -1,9 +1,13 @@
 import H1 from "../components/Typography/H1";
+import H2 from "../components/Typography/H2";
+import CharacterList from "../components/CharacterList";
 
 const Admin = () => {
   return (
     <div>
       <H1>Admin Control Panel</H1>
+      <H2>Players</H2>
+      <CharacterList include="admin" action="link"></CharacterList>
     </div>
   );
 };
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -8,7 +8,7 @@ import firebaseConfig from "../firebaseConfig";
 import { getCurrentRank } from "../Functions/RankFunctions";
 
 interface CharacterListProps {
-  include: string;
+  include: "all" | "admin" | "";
   action: string;
   onClick?: (receiver: string) => void;
 }
@@ -27,7 +27,11 @@ const CharacterList = ({ include, action, onClick }: CharacterListProps) => {
       const characterData = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         username: doc.data().username,
+        familyName: doc.data().familyName,
         xp: doc.data().stats.xp,
+        money: doc.data().stats.money,
+        status: doc.data().status,
+        location: doc.data().location,
       }));
 
       setCharacters(characterData);
@@ -61,10 +65,54 @@ const CharacterList = ({ include, action, onClick }: CharacterListProps) => {
               )}
               {action === "link" && (
                 <Link to={`/profile/${character.id}`}>
-                  <strong>{character.username}</strong>
+                  <strong className="text-white">{character.username}</strong>
                 </Link>
               )}
-              {include === "all" && <> - {getCurrentRank(character.xp)}</>}
+              {include === "all" ||
+                (include === "admin" && <> - {getCurrentRank(character.xp)}</>)}
+
+              {include === "admin" && (
+                <div>
+                  <div className="bg-slate-600 text-slate-300 px-4 py-1 text-sm flex gap-4">
+                    {character.status === "alive" && (
+                      <p>
+                        <i className="fa-solid fa-gun"></i> Kill
+                      </p>
+                    )}
+                    {character.status === "dead" && (
+                      <p>
+                        <i className="fa-solid fa-hand"></i> Ressurect
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-2 bg-slate-700 text-slate-300 px-4 py-2 text-sm flex flex-wrap gap-x-4 gap-y-1">
+                    <p>
+                      Status:{" "}
+                      <span
+                        className={
+                          "capitalize " +
+                          (character.status === "dead"
+                            ? "text-red-400"
+                            : "text-green-400")
+                        }
+                      >
+                        {character.status}
+                      </span>
+                    </p>
+                    <p>
+                      Family:{" "}
+                      {character.familyName ? (
+                        <strong>{character.familyName}</strong>
+                      ) : (
+                        "No family"
+                      )}
+                    </p>
+                    <p>Xp: {character.xp}</p>
+                    <p>Money: {character.money.toLocaleString()}</p>
+                    <p>Location: {character.location}</p>
+                  </div>
+                </div>
+              )}
             </li>
           ))}
         </ul>
