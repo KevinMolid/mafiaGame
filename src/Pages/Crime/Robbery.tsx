af102d850c2749a537ccb4cafabea5e055bda1e5
Kevin Molid <kevinmolid@gmail.com>
2024-10-16 16:24:17 +0200

    Robbery: Added help box

--- src/Pages/Crime/Robbery.tsx
+++ src/Pages/Crime/Robbery.tsx
@@ -1,6 +1,7 @@
 import H1 from "../../components/Typography/H1";
 import Button from "../../components/Button";
 import InfoBox from "../../components/InfoBox";
+import Box from "../../components/Box";
 
 import { useCharacter } from "../../CharacterContext";
 import { Character } from "../../Interfaces/CharacterTypes";
@@ -25,6 +26,7 @@ const Robbery = () => {
   const [messageType, setMessageType] = useState<
     "success" | "failure" | "warning" | "info"
   >("info");
+  const [helpActive, setHelpActive] = useState(false);
   const { character } = useCharacter();
 
   // Function to commit robbery
@@ -62,8 +64,21 @@ const Robbery = () => {
 
       const successChance = Math.random();
       if (successChance > 0.5) {
-        // Example: Transfer money from target to current player
-        const stolenAmount = Math.floor(Math.random() * 500); // Example: rob up to $500
+        // Check if the target has at least $100
+        if (randomTarget.stats.money < 100) {
+          setMessage(
+            `Target ${randomTarget.username} was found but had no money to steal.`
+          );
+          setMessageType("warning");
+          return;
+        }
+
+        // Transfer money from target to current player
+        const stealPercentage = Math.random() * (0.5 - 0.1) + 0.1;
+        const stolenAmount = Math.floor(
+          randomTarget.stats.money * stealPercentage
+        );
+
         setMessage(
           `Success! You robbed ${randomTarget.username} for $${stolenAmount}.`
         );
@@ -79,11 +94,43 @@ const Robbery = () => {
 
   return (
     <section>
-      <H1>Robbery</H1>
-      <p className="mb-2">
+      <div className="flex justify-between">
+        <H1>Robbery</H1>
+        {!helpActive && (
+          <i
+            className="hover:text-yellow-400 text-2xl fa-solid fa-question cursor-pointer"
+            onClick={() => setHelpActive(true)}
+          ></i>
+        )}
+      </div>
+
+      <p className="mb-4">
         Here you can steal money from a random player in your location, or
         attempt to rob a player of your choosing.
       </p>
+
+      {/* Help box */}
+      {helpActive && (
+        <div className="mb-4">
+          <Box>
+            <div className="grid grid-cols-[max-content_auto_max-content] gap-4">
+              <div className="flex justify-center items-center">
+                <i className="text-yellow-400 text-2xl fa-solid fa-question"></i>
+              </div>
+              <small>
+                There is a 50% chance of finding a random target player. If
+                target player has $100 or more, you will steal 10-50% of the
+                targets money.
+              </small>
+              <i
+                className="fa-solid fa-x hover:text-neutral-200 cursor-pointer"
+                onClick={() => setHelpActive(false)}
+              ></i>
+            </div>
+          </Box>
+        </div>
+      )}
+
       {message && <InfoBox type={messageType}>{message}</InfoBox>}
       <form onSubmit={handleSubmit} action="" className="mt-4">
         <Button type="submit">Commit Robbery</Button>
