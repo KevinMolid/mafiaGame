d0c19aa7eadc4441dee2e5a7e523b69064d6f380
Kevin Molid <kevinmolid@gmail.com>
2024-10-17 18:51:57 +0200

    Transparent Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="absolute w-full bottom-0 bg-neutral-950 text-stone-500 text-sm px-8 py-4 text-center">
+    <footer className="absolute w-full bottom-0 bg-transparent text-stone-500 text-sm px-8 py-4 text-center">
       © 2024 Kevin Molid. All rights reserved.
     </footer>
   );
