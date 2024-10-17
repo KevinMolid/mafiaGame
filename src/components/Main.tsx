524ba8261821b8d4e0f66c8be5ab9b4b42a6ae46
Kevin Molid <kevinmolid@gmail.com>
2024-10-17 15:08:04 +0200

    Fixed Main BG issue

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,10 +8,13 @@ interface MainInterface {
 const Main = ({ children, img }: MainInterface) => {
   return (
     <main
-      className={
-        "pb-24 sm:pb-24 p-4 sm:p-12 text-stone-400 flex-grow " +
-        (img && `bg-[url('/${img}.jpg')] bg-cover`)
-      }
+      className={"pb-24 sm:pb-24 p-4 sm:p-12 text-stone-400 flex-grow "}
+      style={{
+        backgroundImage: img ? `url('/${img}.jpg')` : undefined,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
     >
       {children}
     </main>
