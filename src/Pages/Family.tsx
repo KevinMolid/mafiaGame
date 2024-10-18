48722b5a010a07dfabe0587b8114b0ba1e82961e
Kevin Molid <kevinmolid@gmail.com>
2024-10-18 19:20:03 +0200

    Set up Members/Profile/Settings tabs for Family

--- src/Pages/Family.tsx
+++ src/Pages/Family.tsx
@@ -1,6 +1,7 @@
 // Components
 import Main from "../components/Main";
 import H1 from "../components/Typography/H1";
+import H2 from "../components/Typography/H2";
 import InfoBox from "../components/InfoBox";
 import Username from "../components/Typography/Username";
 
@@ -32,6 +33,9 @@ const Family = () => {
   const [family, setFamily] = useState<FamilyData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [activePanel, setActivePanel] = useState<
+    "members" | "profile" | "settings"
+  >("members");
 
   if (!character) return;
 
@@ -72,26 +76,12 @@ const Family = () => {
       {family && (
         <>
           {/* Header */}
-          <div className="flex justify-between items-baseline">
-            <H1>
-              Family: <strong>{family.name}</strong>
-            </H1>
-            {/* Menu button */}
-            <div
-              className="w-10 h-10 bg-neutral-800 hover:bg-neutral-700 hover:text-neutral-200 rounded-md flex justify-center items-center cursor-pointer"
-              onClick={() => console.log("clicked")}
-            >
-              <i className="text-xl fa-solid fa-ellipsis-vertical"></i>
-            </div>
-          </div>
 
-          {error && <p style={{ color: "red" }}>{error}</p>}
+          <H1>
+            Family: <strong>{family.name}</strong>
+          </H1>
 
-          {/* Dropdown */}
-          <div className="bg-neutral-800">
-            <p>Disband family</p>
-          </div>
-
+          {/* Family info */}
           <div className="flex gap-4 mb-2">
             <p>
               Leader:{" "}
@@ -117,22 +107,80 @@ const Family = () => {
             <strong>Family rules:</strong> {family.rules}
           </InfoBox>
 
-          <div>
-            <p>Members:</p>
-            {family.members.map((member) => {
-              return (
-                <p key={member.id}>
-                  <Username
-                    character={{
-                      id: member.id,
-                      username: member.name,
-                    }}
-                  />{" "}
-                  - {member.rank}
-                </p>
-              );
-            })}
-          </div>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+
+          {/* Tabs */}
+          <ul className="mb-8 flex">
+            <li
+              className={
+                " hover:bg-neutral-800 px-4 py-2 max-w-44 border-b-2 border-neutral-700 cursor-pointer " +
+                (activePanel === "members" && "bg-neutral-800 border-white")
+              }
+              onClick={() => setActivePanel("members")}
+            >
+              <p className="text-neutral-200 font-medium">Members</p>
+            </li>
+            <li
+              className={
+                " hover:bg-neutral-800 px-4 py-2 max-w-44 border-b-2 border-neutral-700 cursor-pointer " +
+                (activePanel === "profile" && "bg-neutral-800 border-white")
+              }
+              onClick={() => setActivePanel("profile")}
+            >
+              <p className="text-neutral-200 font-medium">Profile</p>
+            </li>
+            <li
+              className={
+                " hover:bg-neutral-800 px-4 py-2 max-w-44 border-b-2 border-neutral-700 cursor-pointer " +
+                (activePanel === "settings" && "bg-neutral-800 border-white")
+              }
+              onClick={() => setActivePanel("settings")}
+            >
+              <p className="text-neutral-200 font-medium">Settings</p>
+            </li>
+          </ul>
+
+          {/* Member panel */}
+          {activePanel === "members" && (
+            <div>
+              <H2>Members</H2>
+              {family.members.map((member) => {
+                return (
+                  <p key={member.id}>
+                    <Username
+                      character={{
+                        id: member.id,
+                        username: member.name,
+                      }}
+                    />{" "}
+                    - {member.rank}
+                  </p>
+                );
+              })}
+            </div>
+          )}
+
+          {/* Profile panel */}
+          {activePanel === "profile" && (
+            <div>
+              <H2>Profile</H2>
+              <p>This is the family profile text</p>
+            </div>
+          )}
+
+          {/* Settings panel */}
+          {activePanel === "settings" && (
+            <div>
+              <H2>Settings</H2>
+              <p>Invite player</p>
+              <p>Assign roles</p>
+              <hr className="my-2 border-neutral-600" />
+              <p>Edit Family rules</p>
+              <p>Edit Family profile</p>
+              <hr className="my-2 border-neutral-600" />
+              <p className="text-red-400">Disband family</p>
+            </div>
+          )}
         </>
       )}
     </Main>
