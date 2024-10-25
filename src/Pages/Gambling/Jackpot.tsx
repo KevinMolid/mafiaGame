d5b00265245aa6a7f5b8f48f6b8ddecf163697f1
Kevin Molid <kevinmolid@gmail.com>
2024-10-25 10:43:47 +0200

    Fullført Jackpot

--- src/Pages/Gambling/Jackpot.tsx
+++ src/Pages/Gambling/Jackpot.tsx
@@ -1,67 +1,141 @@
 import { useState } from "react";
 import Main from "../../components/Main";
 import H1 from "../../components/Typography/H1";
+import H2 from "../../components/Typography/H2";
 import Button from "../../components/Button";
+import InfoBox from "../../components/InfoBox";
+
+import { getFirestore, doc, updateDoc } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "../../firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+import { useCharacter } from "../../CharacterContext";
 
 const Jackpot = () => {
-  const [reels, setReels] = useState([0, 0, 0]);
-  const [result, setResult] = useState("");
-  const [balance, setBalance] = useState(1000);
-  const betAmount = 100;
+  const { character } = useCharacter();
+  const [reels, setReels] = useState<Array<number>>([0, 0, 0]);
+  const [message, setMessage] = useState<string>("");
+  const [messageType, setMessageType] = useState<
+    "success" | "failure" | "info" | "warning"
+  >("info");
+  const [betAmount, setBetAmount] = useState<number>(0);
+
+  if (!character) return;
 
   // Function to spin the reels
   const spinReels = () => {
-    if (balance < betAmount) {
-      setResult("Not enough money to spin!");
+    if (betAmount < 100) {
+      setMessageType("warning");
+      setMessage("Du må satse minst $100!");
+      return;
+    }
+
+    if (character?.stats.money < betAmount) {
+      setMessageType("warning");
+      setMessage("Du har ikke så mye penger!");
       return;
     }
 
     const newReels = [
-      Math.floor(Math.random() * 9) + 1, // Random number between 1 and 3 for reel 1
-      Math.floor(Math.random() * 9) + 1, // Random number between 1 and 3 for reel 2
-      Math.floor(Math.random() * 9) + 1, // Random number between 1 and 3 for reel 3
+      Math.floor(Math.random() * 9) + 1,
+      Math.floor(Math.random() * 9) + 1,
+      Math.floor(Math.random() * 9) + 1,
     ];
     setReels(newReels);
     evaluateSpin(newReels);
   };
 
   // Function to evaluate the result of the spin
-  const evaluateSpin = (reels: number[]) => {
-    if (reels[0] === reels[1] && reels[1] === reels[2]) {
-      setBalance(balance + betAmount * 5); // 5x payout for matching all 3
-      setResult("Jackpot! You won 5x your bet!");
+  const evaluateSpin = async (reels: number[]) => {
+    let newMoney = character.stats.money;
+
+    if (reels[0] === reels[1] && reels[1] === reels[2] && reels[2] === 7) {
+      newMoney += betAmount * 150; // 150x payout for 7 7 7
+      setMessageType("success");
+      setMessage(`Jackpot! Du vant $${(betAmount * 150).toLocaleString()}!`);
+    } else if (reels[0] === reels[1] && reels[1] === reels[2]) {
+      newMoney += betAmount * 15; // 10x payout for matching all 3
+      setMessageType("success");
+      setMessage(
+        `Du fikk 3 like og vant $${(betAmount * 15).toLocaleString()}!`
+      );
     } else if (
       reels[0] === reels[1] ||
       reels[1] === reels[2] ||
       reels[0] === reels[2]
     ) {
-      setBalance(balance + betAmount * 2); // 2x payout for 2 matches
-      setResult("You won 2x your bet!");
+      newMoney += betAmount * 2; // 2x payout for 2 matches
+      setMessageType("success");
+      setMessage(
+        `Du fikk 2 like og vant $${(betAmount * 2).toLocaleString()}!`
+      );
     } else {
-      setBalance(balance - betAmount); // Deduct bet amount if no match
-      setResult("Du tapte. Prøv igjen!");
+      newMoney -= betAmount; // Deduct bet amount if no match
+      setMessageType("failure");
+      setMessage(`Du tapte $${betAmount}. Prøv igjen!`);
+    }
+
+    try {
+      const characterRef = doc(db, "Characters", character.id);
+      // Update values in Firestore
+      await updateDoc(characterRef, {
+        "stats.money": newMoney,
+      });
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <Main>
       <H1>Jackpot</H1>
+
+      {message && <InfoBox type={messageType}>{message}</InfoBox>}
+
       <div className="flex flex-col gap-4">
-        <p>Balance: ${balance}</p>
-        <div className="flex gap-4">
-          <div>
-            <i className={`fa-solid fa-${reels[0]}`}></i>
+        <H2>Hvor mye vil du satse?</H2>
+        <input
+          className="bg-neutral-700 py-2 px-4 text-white placeholder-neutral-400"
+          type="number"
+          placeholder="Skriv inn beløp"
+          onChange={(e) => setBetAmount(Number(e.target.value))}
+        />
+
+        <div className="w-[max-content]">
+          <div className="flex gap-1 bg-neutral-900 border w-[max-content] px-8 py-4">
+            <div className="w-12 py-4 flex justify-center items-center bg-neutral-200 border border-neutral-700">
+              <i
+                className={
+                  `text-5xl fa-solid fa-${reels[0]} ` +
+                  (reels[0] === 7 ? "text-red-600" : "text-neutral-700")
+                }
+              ></i>
+            </div>
+            <div className="w-12 py-4 flex justify-center items-center bg-neutral-200 border border-neutral-700">
+              <i
+                className={
+                  `text-5xl fa-solid fa-${reels[1]} ` +
+                  (reels[1] === 7 ? "text-red-600" : "text-neutral-700")
+                }
+              ></i>
+            </div>{" "}
+            <div className="w-12 py-4 flex justify-center items-center bg-neutral-200 border border-neutral-700">
+              <i
+                className={
+                  `text-5xl fa-solid fa-${reels[2]} ` +
+                  (reels[2] === 7 ? "text-red-600" : "text-neutral-700")
+                }
+              ></i>
+            </div>
           </div>
-          <div>
-            <i className={`fa-solid fa-${reels[1]}`}></i>
-          </div>{" "}
-          <div>
-            <i className={`fa-solid fa-${reels[2]}`}></i>
+          <div className="bg-neutral-900 border flex justify-center p-2">
+            <Button onClick={spinReels}>Spill!</Button>
           </div>
         </div>
-
-        <Button onClick={spinReels}>Spinn!</Button>
-        <p>{result}</p>
       </div>
     </Main>
   );
