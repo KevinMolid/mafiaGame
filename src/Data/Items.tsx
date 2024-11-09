b9581a9d2a9845af4c628ba08b5fdaf332435dfd
Kevin Molid <kevinmolid@gmail.com>
2024-11-09 02:52:31 +0100

    Bilder til items

--- src/Data/Items.tsx
+++ src/Data/Items.tsx
@@ -1,6 +1,12 @@
-import fedora1 from "/images/items/Fedora1.png";
 import h1 from "/images/items/h1.jpg";
-import f2 from "/images/items/f2.jpg";
+import h2 from "/images/items/h2.jpg";
+import h3 from "/images/items/h3.jpg";
+import h4 from "/images/items/h4.jpg";
+import h5 from "/images/items/h5.jpg";
+import h6 from "/images/items/h6.jpg";
+import h7 from "/images/items/h7.jpg";
+import h8 from "/images/items/h8.jpg";
+import h9 from "/images/items/h9.jpg";
 
 const Items = [
   // Tier 1
@@ -9,29 +15,29 @@ const Items = [
     rarity: "common",
     value: 150,
     hp: 5,
-    img: fedora1,
+    img: h1,
   },
   {
     name: "Biltmore Fedora",
     rarity: "common",
     value: 250,
     hp: 10,
-    img: fedora1,
-  },
-  {
-    name: "Stetson Fedora",
-    rarity: "common",
-    value: 300,
-    hp: 15,
-    img: fedora1,
+    img: h2,
   },
   // Tier 2
+  {
+    name: "Stetson Fedora",
+    rarity: "uncommon",
+    value: 300,
+    hp: 15,
+    img: h3,
+  },
   {
     name: "Resistol Fedora",
     rarity: "uncommon",
     value: 500,
     hp: 25,
-    img: fedora1,
+    img: h4,
   },
   // Tier 3
   {
@@ -39,15 +45,37 @@ const Items = [
     rarity: "rare",
     value: 600,
     hp: 35,
-    img: h1,
+    img: h5,
+  },
+  {
+    name: "Lock & Co Fedora",
+    rarity: "rare",
+    value: 1000,
+    hp: 50,
+    img: h6,
   },
   // Tier 4
   {
-    name: "Lock & Co Fedora",
+    name: "Rosa Fedora",
     rarity: "epic",
     value: 1000,
     hp: 50,
-    img: f2,
+    img: h7,
+  },
+  {
+    name: "Pimp Fedora",
+    rarity: "epic",
+    value: 1000,
+    hp: 50,
+    img: h8,
+  },
+  // Tier 5
+  {
+    name: "Bling Fedora",
+    rarity: "legendary",
+    value: 1000,
+    hp: 50,
+    img: h9,
   },
 ];
 
