bd62dba29a4be3b193c7c94a47ea4b8c113e5b01
Kevin Molid <kevinmolid@gmail.com>
2024-11-10 11:20:50 +0100

    Fikset tekst-farge på knapp

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,23 +19,23 @@ const Button = ({
     <button
       type={type}
       className={
-        "text-white font-medium rounded-full " +
+        "font-medium rounded-full " +
         (size === "small"
           ? "px-2 py-1 min-w-16 "
           : size === "square"
           ? "w-10 h-10 "
           : "px-4 py-2 min-w-32 ") +
         (style === "secondary"
-          ? "bg-neutral-600 hover:bg-neutral-700 "
+          ? "bg-neutral-600 hover:bg-neutral-700 text-white "
           : style === "help"
-          ? "bg-neutral-900 border-2 border-neutral-600 hover:border-neutral-500 text-neutral-500 hover:text-neutral-400"
+          ? "bg-neutral-900 border-2 border-neutral-600 hover:border-neutral-500 text-neutral-500 hover:text-neutral-400 "
           : style === "helpActive"
           ? "bg-neutral-900 border-2 border-yellow-400 text-yellow-400 hover:bg-neutral-950 "
           : style === "exit"
-          ? "bg-neutral-800 hover:bg-neutral-700 text-neutral-500 hover:text-neutral-400"
+          ? "bg-neutral-800 hover:bg-neutral-700 text-neutral-500 hover:text-neutral-400 "
           : style === "danger"
-          ? "bg-red-700 hover:bg-red-800"
-          : "bg-sky-600 hover:bg-sky-700")
+          ? "bg-red-700 hover:bg-red-800 text-white"
+          : "bg-sky-600 hover:bg-sky-700 text-white")
       }
       onClick={onClick}
     >
