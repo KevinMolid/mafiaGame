a78838eb0707fee4cf789ce5c2b6a2f622e8a832
Kevin Molid <kevinmolid@gmail.com>
2024-11-15 22:12:32 +0100

    Utvidet spillguiden

--- src/Functions/RankFunctions.tsx
+++ src/Functions/RankFunctions.tsx
@@ -1,28 +1,55 @@
 // Define rank thresholds
 export const ranks = [
-  { name: "Nybegynner", minXP: 0, maxXP: 50 },
-  { name: "Pøbel", minXP: 50, maxXP: 150 },
-  { name: "Svindler", minXP: 150, maxXP: 400 },
-  { name: "Gangster", minXP: 400, maxXP: 1000 },
-  { name: "Torpedo", minXP: 1000, maxXP: 2500 },
-  { name: "Leiemorder", minXP: 2500, maxXP: 5000 },
-  { name: "Løytnant", minXP: 5000, maxXP: 10000 },
-  { name: "Kaptein", minXP: 10000, maxXP: 20000 },
-  { name: "Don", minXP: 20000, maxXP: 40000 },
-  { name: "Gudfar", minXP: 40000, maxXP: Infinity },
+  { name: "Nybegynner", minXP: 0, maxXP: 50, perks: "Kan utføre kriminalitet" },
+  {
+    name: "Pøbel",
+    minXP: 50,
+    maxXP: 150,
+    perks: "Kan utføre biltyveri, sende meldinger",
+  },
+  {
+    name: "Svindler",
+    minXP: 150,
+    maxXP: 400,
+    perks: "Kan rane spillere, skrive i forum",
+  },
+  {
+    name: "Gangster",
+    minXP: 400,
+    maxXP: 1000,
+    perks: "Kan bli medlem av familie, delta på organisert ran",
+  },
+  { name: "Torpedo", minXP: 1000, maxXP: 2500, perks: "Kan angripe spillere" },
+  { name: "Leiemorder", minXP: 2500, maxXP: 5000, perks: "Kan drepe spillere" },
+  {
+    name: "Løytnant",
+    minXP: 5000,
+    maxXP: 10000,
+    perks: "Kan starte organisert ran",
+  },
+  {
+    name: "Kaptein",
+    minXP: 10000,
+    maxXP: 20000,
+    perks: "Kan opprette familie",
+  },
+  { name: "Gudfar", minXP: 20000, maxXP: 40000 },
+  { name: "Don", minXP: 40000, maxXP: Infinity },
 ];
 
 // Define rank thresholds
-const moneyRanks = [
-  { name: "Fattig", minAmount: 0, maxAmount: 999 },
+export const moneyRanks = [
+  { name: "Raka fant", minAmount: 0, maxAmount: 9 },
+  { name: "Lutfattig", minAmount: 10, maxAmount: 99 },
+  { name: "Fattig", minAmount: 100, maxAmount: 999 },
   { name: "Sliter", minAmount: 1000, maxAmount: 9999 },
   { name: "Arbeidsledig", minAmount: 10000, maxAmount: 99999 },
   { name: "Arbeidende", minAmount: 100000, maxAmount: 999999 },
   { name: "Millionær", minAmount: 1000000, maxAmount: 9999999 },
-  { name: "Multimillionær", minAmount: 10000000, maxAmount: 99999999 },
-  { name: "Mangemillionær", minAmount: 100000000, maxAmount: 999999999 },
+  { name: "Mangemillionær", minAmount: 10000000, maxAmount: 99999999 },
+  { name: "Businessmann", minAmount: 100000000, maxAmount: 999999999 },
   { name: "Milliardær", minAmount: 1000000000, maxAmount: 9999999999 },
-  { name: "Multimilliardær", minAmount: 10000000000, maxAmount: 99999999999 },
+  { name: "Mangemilliardær", minAmount: 10000000000, maxAmount: 99999999999 },
   { name: "Mogul", minAmount: 100000000000, maxAmount: 999999999999 },
   { name: "Supermogul", minAmount: 1000000000000, maxAmount: Infinity },
 ];
--- src/Pages/GameGuide.tsx
+++ src/Pages/GameGuide.tsx
@@ -4,7 +4,7 @@ import H1 from "../components/Typography/H1";
 import H2 from "../components/Typography/H2";
 import H3 from "../components/Typography/H3";
 
-import { ranks } from "../Functions/RankFunctions";
+import { ranks, moneyRanks } from "../Functions/RankFunctions";
 
 const GameGuide = () => {
   return (
@@ -12,32 +12,93 @@ const GameGuide = () => {
       <H1>Spillguiden</H1>
       <div className="flex flex-col gap-4">
         <p>
-          <strong>Mafia Reign</strong> er et spennende flerspillerspill der du
-          bygger ditt kriminelle imperium i skyggene av fem storbyer.
+          <strong>Den siste Don</strong> er et flerspillerspill der du bygger
+          ditt kriminelle imperium i skyggene av fem storbyer.
         </p>
         <p>
-          Stig i gradene, bygg din mafiafamilie og få innflytelse i byer som New
-          York og Tokyo. Men husk, det finnes alltid noen sterkere i skyggene.
-          Hvor langt er du villig til å gå for å herske?
+          Stig i gradene, bygg din familie og få innflytelse. Men husk, det
+          finnes alltid noen sterkere i skyggene. Hvor langt er du villig til å
+          gå for å herske?
         </p>
         <H2>Kriminelle handlinger</H2>
+        <H3>Kriminalitet</H3>
         <p>
-          Gjennom å utføre <strong>kriminelle handlinger</strong> vil du tjene
-          erfaringspoeng (xp). Basert på dine erfaringspoeng vil du få tildelt
-          en rank som forteller hvor langt du har kommet i spillet.
+          Utfør enkle kriminelle handlinger som gir deg erfaring (xp) og penger.
+          De ulike alternativene har ulik sjanse for å lykkes, og gir ulik
+          mengde erfaring og penger.
         </p>
         <p>
-          Kriminelle handlinger som gir deg erfaringspoeng (xp) er Kriminalitet,
-          Biltyveri, Ran spiller og Drep spiller.
+          <span className="text-neutral-200">Lommetyveri</span>: 90% sjanse, 4xp
+          <br />
+          <span className="text-neutral-200">Herverk</span>:: 85% sjanse, 5xp{" "}
+          <br />
+          <span className="text-neutral-200">Stjel verdisaker</span>:: 80%
+          sjanse, 6xp <br />
+          <span className="text-neutral-200">Ran butikk</span>:: 75% sjanse, 7xp
         </p>
-
+        <H3>Biltyveri</H3>
+        <H3>Ran spiller</H3>
+        <p>
+          Du kan rane en spiller dersom dere begge befinner dere i samme by. Ved
+          vellykket ran vil du stjele 10-50% av spillerens utestående penger.
+        </p>
+        <p>
+          <strong className="text-neutral-200">
+            Rane en tilfeldig spiller
+          </strong>
+          <br />
+          Forsøk å rane en tilfeldig spiller i samme by.
+        </p>
+        <p>
+          <strong className="text-neutral-200">Rane en bestemt spiller</strong>
+          <br />
+          Skriv in brukernavnet på spilleren du vil forsøke å rane. Dersom
+          spilleren er i samme by er det 50% sjanse for at du finner spilleren.
+          Deretter er det 75% sjanse for vellykket ran.
+        </p>
+        <H3>Drep spiller</H3>
         <H2>Ranksystemet</H2>
         <p>De ulike rankene låser opp ulike funksjoner og utstyr i spillet.</p>
+        {/* Ranks */}
         <H3>Rankene</H3>
         <ul>
+          <li
+            key="rank-headings"
+            className="grid grid-cols-[15px_100px_auto] gap-4 font-bold text-neutral-200 border-b border-neutral-600"
+          >
+            <p></p>
+            <p>Rank</p>
+            <p>Låser opp</p>
+          </li>
           {ranks.map((rank, index) => (
-            <li key={rank.name}>
-              {index + 1}. {rank.name}
+            <li
+              key={rank.name}
+              className="grid grid-cols-[15px_100px_auto] gap-4"
+            >
+              <p>{index + 1}. </p>
+              <p className="text-neutral-200">{rank.name}</p>
+              <p>{rank.perks}</p>
+            </li>
+          ))}
+        </ul>
+        <H3>Pengerankene</H3>
+        <ul>
+          <li
+            key="rank-headings"
+            className="grid grid-cols-[15px_120px_auto] gap-4 font-bold text-neutral-200 border-b border-neutral-600"
+          >
+            <p></p>
+            <p>Rank</p>
+            <p>Penger</p>
+          </li>
+          {moneyRanks.map((rank, index) => (
+            <li
+              key={rank.name}
+              className="grid grid-cols-[15px_120px_auto] gap-4"
+            >
+              <p>{index + 1}. </p>
+              <p className="text-neutral-200">{rank.name}</p>
+              <p>${rank.minAmount.toLocaleString()} +</p>
             </li>
           ))}
         </ul>
