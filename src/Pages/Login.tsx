a35a0ef7578377351500bef1cc9f0d25d1a5b4d2
Kevin Molid <kevinmolid@gmail.com>
2024-11-17 17:11:20 +0100

    Prøver å sette opp Facebook auth, trenger tilgang

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -15,6 +15,7 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
+  FacebookAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
 
@@ -50,7 +51,19 @@ const Login = () => {
     });
   }
 
-  /* Handle Google Login */
+  /* Login with Facebook */
+  function logInWithFacebook() {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error.code);
+      });
+  }
+
+  /* Login with Google */
   function logInWithGoogle() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
@@ -64,22 +77,26 @@ const Login = () => {
 
   return (
     <Main img="Mafia">
-      <div className="w-5/6 sm:w-2/3 max-w-[500px] flex flex-col relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center">
+      <div className="w-full sm:w-5/6 md:w-2/3 max-w-[500px] flex flex-col relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center">
         <small className="bg-neutral-900 px-4 py-2 w-fit rounded-t-xl border-t border-x border-neutral-500">
           v. <strong>Alpha</strong>
         </small>
         <div className="bg-neutral-900/80 border border-neutral-500 p-6 rounded-lg flex flex-col gap-4 w-full">
-          <H1>Logg inn</H1>
-          <Button style="secondary" onClick={logInWithGoogle}>
-            <div className="flex justify-center gap-2">
-              <img
-                src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png"
-                alt="Google logo"
-                className="size-6"
-              />
-              <p className="mr-4">Logg inn</p>
-            </div>
-          </Button>
+          <div className="flex flex-col gap-2">
+            <H1>Logg inn med</H1>
+
+            {/* Google login */}
+            <Button style="secondary" onClick={logInWithGoogle}>
+              <div className="flex justify-center gap-2">
+                <img
+                  src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png"
+                  alt="Google logo"
+                  className="size-6"
+                />
+                <p className="mr-4">Google</p>
+              </div>
+            </Button>
+          </div>
 
           <form action="" className="flex flex-col gap-2">
             <div className="flex flex-col">
