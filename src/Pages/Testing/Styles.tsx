c244f47f0ff1aec40e3b95dc4e3c5448e0208384
Kevin Molid <kevinmolid@gmail.com>
2024-11-18 21:46:26 +0100

    Ny stil for knapper

--- src/Pages/Testing/Styles.tsx
+++ src/Pages/Testing/Styles.tsx
@@ -4,7 +4,20 @@ import Button from "../../components/Button";
 const Styles = () => {
   return (
     <Main>
-      <Button>Test</Button>
+      <div className="flex gap-2 flex-wrap">
+        <Button>Test</Button>
+        <Button style="secondary">Test</Button>
+        <Button style="black">Test</Button>
+        <Button style="danger">Test</Button>
+        <Button style="help">Test</Button>
+        <Button style="helpActive">Test</Button>
+        <Button style="exit">Test</Button>
+
+        <Button style="realistic">Test</Button>
+        <Button>Test</Button>
+        <Button>Test</Button>
+        <Button>Test</Button>
+      </div>
     </Main>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,6 +11,7 @@ interface ButtonInterface {
     | "help"
     | "helpActive"
     | "danger"
+    | "realistic"
     | "exit";
   size?: "small" | "normal" | "square";
 }
@@ -33,18 +34,20 @@ const Button = ({
           ? "w-10 h-10 "
           : "px-4 py-2 min-w-32 ") +
         (style === "secondary"
-          ? "bg-neutral-600 hover:bg-neutral-700 text-white "
+          ? "bg-neutral-900 border-2 border-neutral-400 text-white hover:bg-neutral-950"
           : style === "black"
-          ? "bg-neutral-900 border-2 border-neutral-600 hover:bg-neutral-950 text-white "
+          ? "bg-neutral-900 border-2 border-neutral-600 hover:bg-neutral-950 text-white"
           : style === "help"
-          ? "bg-neutral-900 border-2 border-neutral-600 hover:border-neutral-500 text-neutral-500 hover:text-neutral-400 "
+          ? "bg-neutral-900 border-2 border-neutral-600 hover:border-neutral-500 text-neutral-500 hover:text-neutral-400"
           : style === "helpActive"
-          ? "bg-neutral-900 border-2 border-yellow-400 text-yellow-400 hover:bg-neutral-950 "
+          ? "bg-neutral-900 border-2 border-yellow-400 text-white hover:bg-neutral-950"
           : style === "exit"
-          ? "bg-neutral-800 hover:bg-neutral-700 text-neutral-500 hover:text-neutral-400 "
+          ? "bg-neutral-800 hover:bg-neutral-700 text-neutral-500 hover:text-neutral-400"
           : style === "danger"
-          ? "bg-neutral-900 border-2 border-red-400 text-red-400 hover:bg-neutral-950"
-          : "bg-sky-600 hover:bg-sky-700 text-white")
+          ? "bg-neutral-900 border-2 border-red-400 text-white hover:bg-neutral-950"
+          : style === "realistic"
+          ? "bg-gradient-to-br from-neutral-00 to-neutral-700 border-2 border-black text-white shadow-[inset_2px_2px_0px_#7d7c7e,inset_-2px_-2px_0px_#171717]"
+          : "bg-neutral-900 border-2 border-sky-400 text-white hover:bg-neutral-950")
       }
       onClick={onClick}
     >
