7fb1851923ca77ad7f96d13fe511ce2fe0b0a128
Kevin Molid <kevinmolid@gmail.com>
2024-11-18 23:47:08 +0100

    Endret layout for handlinger i Admin

--- src/Pages/Admin.tsx
+++ src/Pages/Admin.tsx
@@ -3,7 +3,7 @@ import Main from "../components/Main";
 import H1 from "../components/Typography/H1";
 import H2 from "../components/Typography/H2";
 import CharacterList from "../components/CharacterList";
-import SidebarLink from "../components/SidebarLink";
+import { Link } from "react-router-dom";
 
 import { useAuth } from "../AuthContext";
 
@@ -19,14 +19,22 @@ const Admin = () => {
   return (
     <Main>
       <H1>Kontrollpanel</H1>
-      <div className="border p-4 border-neutral-600 mb-4">
+      <div className="border p-4 border-neutral-600 mb-4 gap-2">
         <H2>Handlinger</H2>
-        <SidebarLink to="/velgspiller" icon="people-group">
-          Velg spillkarakter
-        </SidebarLink>
-        <SidebarLink to="/nyspiller" icon="user-plus">
-          Ny spillkarakter
-        </SidebarLink>
+        <div className="flex flex-wrap gap-2">
+          <Link to="/velgspiller">
+            <div className="flex gap-2 items-center bg-neutral-800 hover:bg-neutral-700 px-4 py-2 rounded-lg w-[max-content]">
+              <i className="fa-solid fa-user-group"></i>
+              <p>Velg spillkarakter</p>
+            </div>
+          </Link>
+          <Link to="/nyspiller">
+            <div className="flex gap-2 items-center bg-neutral-800 hover:bg-neutral-700 px-4 py-2 rounded-lg w-[max-content]">
+              <i className="fa-solid fa-user-plus"></i>
+              <p>Ny spillkarakter</p>
+            </div>
+          </Link>
+        </div>
       </div>
 
       <H2>Spillere</H2>
