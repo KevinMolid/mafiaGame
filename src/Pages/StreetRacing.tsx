71a2756e5b57c05a146bebb9342b2b1ad8b81c9e
Kevin Molid <kevinmolid@gmail.com>
2024-11-20 18:57:30 +0100

    Fjernet bilder fra StreetRacing

--- src/Pages/StreetRacing.tsx
+++ src/Pages/StreetRacing.tsx
@@ -4,12 +4,6 @@ import H2 from "../components/Typography/H2";
 import Button from "../components/Button";
 import Box from "../components/Box";
 
-import Super from "/images/Super.jpg";
-import Aklasse from "/images/A-class.jpg";
-import Bklasse from "/images/B-class.jpg";
-import Cklasse from "/images/C-class.jpg";
-import Dklasse from "/images/D-class.jpg";
-
 const StreetRacing = () => {
   return (
     <Main>
@@ -19,89 +13,64 @@ const StreetRacing = () => {
       </p>
       <div className="flex gap-4 flex-wrap">
         {/* D-klasse */}
-        <div>
-          <img
-            src={Dklasse}
-            className="border border-b-2 border-neutral-600 border-b-neutral-400"
-          />
-          <Box>
-            <div className="flex justify-between items-end">
-              <div>
-                <H2>D-klasse</H2>
-                <p>Velg bil</p>
-              </div>
-              <Button>Delta</Button>
+
+        <Box>
+          <div className="flex justify-between items-end">
+            <div>
+              <H2>D-klasse</H2>
+              <p>Velg bil</p>
             </div>
-          </Box>
-        </div>
+            <Button>Delta</Button>
+          </div>
+        </Box>
 
         {/* C-klasse */}
-        <div>
-          <img
-            src={Cklasse}
-            className="border border-b-2 border-neutral-600 border-b-green-400"
-          />
-          <Box>
-            <div className="flex justify-between items-end">
-              <div>
-                <H2>C-klasse</H2>
-                <p>Velg bil</p>
-              </div>
-              <Button>Delta</Button>
+
+        <Box>
+          <div className="flex justify-between items-end">
+            <div>
+              <H2>C-klasse</H2>
+              <p>Velg bil</p>
             </div>
-          </Box>
-        </div>
+            <Button>Delta</Button>
+          </div>
+        </Box>
 
         {/* B-klasse */}
-        <div>
-          <img
-            src={Bklasse}
-            className="border border-b-2 border-neutral-600 border-b-blue-400"
-          />
-          <Box>
-            <div className="flex justify-between items-end">
-              <div>
-                <H2>B-klasse</H2>
-                <p>Velg bil</p>
-              </div>
-              <Button>Delta</Button>
+
+        <Box>
+          <div className="flex justify-between items-end">
+            <div>
+              <H2>B-klasse</H2>
+              <p>Velg bil</p>
             </div>
-          </Box>
-        </div>
+            <Button>Delta</Button>
+          </div>
+        </Box>
 
         {/* A-klasse */}
-        <div>
-          <img
-            src={Aklasse}
-            className="border border-b-2 border-neutral-600 border-b-purple-400"
-          />
-          <Box>
-            <div className="flex justify-between items-end">
-              <div>
-                <H2>A-klasse</H2>
-                <p>Velg bil</p>
-              </div>
-              <Button>Delta</Button>
+
+        <Box>
+          <div className="flex justify-between items-end">
+            <div>
+              <H2>A-klasse</H2>
+              <p>Velg bil</p>
             </div>
-          </Box>
-        </div>
+            <Button>Delta</Button>
+          </div>
+        </Box>
 
         {/* S-klasse */}
-        <div>
-          <img
-            src={Super}
-            className="border border-b-2 border-neutral-600 border-b-yellow-400"
-          />
-          <Box>
-            <div className="flex justify-between items-end">
-              <div>
-                <H2>S-klasse</H2>
-                <p>Velg bil</p>
-              </div>
-              <Button>Delta</Button>
+
+        <Box>
+          <div className="flex justify-between items-end">
+            <div>
+              <H2>S-klasse</H2>
+              <p>Velg bil</p>
             </div>
-          </Box>
-        </div>
+            <Button>Delta</Button>
+          </div>
+        </Box>
       </div>
     </Main>
   );
