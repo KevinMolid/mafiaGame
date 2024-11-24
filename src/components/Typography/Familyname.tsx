310ac378b90a1932c9d660d7382b28d7d9cd1bd5
Kevin Molid <kevinmolid@gmail.com>
2024-11-24 14:41:14 +0100

    Endret farge på Familyname

--- src/components/Typography/Familyname.tsx
+++ src/components/Typography/Familyname.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Familyname = ({ family }: any) => {
   return (
     <Link to={`/familie/profil/${family.id}`}>
-      <strong className="text-white hover:underline">{family.name}</strong>
+      <strong className="text-neutral-400 hover:underline">
+        {family.name}
+      </strong>
     </Link>
   );
 };
