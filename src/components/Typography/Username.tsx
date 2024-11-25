56c4373c7e55958b0df742a4baa43333320fbfd8
Kevin Molid <kevinmolid@gmail.com>
2024-11-25 18:37:56 +0100

    La til ikoner for selv, venner og svarteliste

--- src/components/Typography/Username.tsx
+++ src/components/Typography/Username.tsx
@@ -4,10 +4,32 @@ import { useCharacter } from "../../CharacterContext";
 
 const Username = ({ character }: any) => {
   const { userCharacter } = useCharacter();
-  console.log(userCharacter);
+
+  const friendIDs = userCharacter?.friends.map((friend: any) => {
+    return friend.id;
+  });
+
+  const blacklistIDs = userCharacter?.blacklist.map((player: any) => {
+    return player.id;
+  });
 
   return (
     <Link to={`/profil/${character.id}`}>
+      {userCharacter && character.id === userCharacter.id && (
+        <span className="text-sky-400">
+          <i className="fa-solid fa-user"></i>{" "}
+        </span>
+      )}
+      {userCharacter && friendIDs.includes(character.id) && (
+        <span className="text-green-400">
+          <i className="fa-solid fa-user"></i>{" "}
+        </span>
+      )}
+      {userCharacter && blacklistIDs.includes(character.id) && (
+        <span className="text-red-400">
+          <i className="fa-solid fa-skull-crossbones"></i>{" "}
+        </span>
+      )}
       <strong className="text-white hover:underline">
         {character.username}
       </strong>
