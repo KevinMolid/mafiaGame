fe1094928811aeb9ee69c6388453c701b86ee023
Kevin Molid <kevinmolid@gmail.com>
2025-09-29 11:38:00 +0200

    1 dager -> 1 dag

--- src/Functions/TimeFunctions.tsx
+++ src/Functions/TimeFunctions.tsx
@@ -6,14 +6,18 @@ export const timeAgo = (timestamp: number) => {
   if (secondsElapsed < 60) return `${secondsElapsed} sek`;
   const minutes = Math.floor(secondsElapsed / 60);
   if (minutes < 60) return `${minutes} min`;
+
   const hours = Math.floor(minutes / 60);
-  if (hours < 24) return `${hours} timer`;
+  if (hours < 24) return hours === 1 ? `1 time` : `${hours} timer`;
+
   const days = Math.floor(hours / 24);
-  if (days < 30) return `${days} dager`;
+  if (days < 30) return days === 1 ? `1 dag` : `${days} dager`;
+
   const months = Math.floor(days / 30);
-  if (months < 12) return `${months} måneder`;
-  const years = Math.floor(months / 12);
-  return `${years} år`;
+  if (months < 12) return months === 1 ? `1 måned` : `${months} måneder`;
+
+  const years = Math.floor(days / 365);
+  return years === 1 ? `1 år` : `${years} år`;
 };
 
 export default timeAgo;
