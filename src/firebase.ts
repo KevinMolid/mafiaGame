04be1a2fa7eba400db1b49031b235683153baf57
Kevin Molid <kevinmolid@gmail.com>
2025-12-12 23:47:35 +0100

    Added reCAPTCHA

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
 import { getDatabase } from "firebase/database";
 import firebaseConfig from "./firebaseConfig";
 import { initServerTime } from "./Functions/serverTime";
@@ -11,3 +13,10 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
 initServerTime(app);
+
+if (typeof window !== "undefined") {
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
