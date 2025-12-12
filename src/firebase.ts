9a0098736aa715f760a6f58ede922b3f13083a83
Kevin Molid <kevinmolid@gmail.com>
2025-12-13 00:02:20 +0100

    reCaptcha for prod

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,7 +14,14 @@ export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
 initServerTime(app);
 
+// App Check
 if (typeof window !== "undefined") {
+  // DEV: use debug token so you don't get locked out while testing
+  if (import.meta.env.DEV) {
+    // prints a token in console the first time
+    (self as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+  }
+
   initializeAppCheck(app, {
     provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY),
     isTokenAutoRefreshEnabled: true,
